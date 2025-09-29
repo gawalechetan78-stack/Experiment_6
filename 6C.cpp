1ca7891cd4cfee5c907dc0107b01cbd818244591
gawalechetan78-stack <gawalechetan78@gmail.com>
2025-09-29 22:49:13 +0530

    6C.cpp

---
+++ 6C.cpp
@@ -0,0 +1,24 @@
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    for(int i = 0; i < 11; i++) {
+        if(i % 2 == 0) {
+            cout << i << " ";
+        }
+    }
+
+    cout << endl;
+
+    return 0;
+}
+
+/* Output 
+
+0 2 4 6 8 10
+
+*/
